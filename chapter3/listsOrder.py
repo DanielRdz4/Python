30f78f3d53235bb786f7c6fb233d294b6a822cdf
Daniel <138172621+DanielRdz4@users.noreply.github.com>
2025-03-07 20:26:56 -0600

    Ultimos ejercicios del capítulo 3

---
+++ chapter3/listsOrder.py
@@ -0,0 +1,12 @@
+#Programa ejemplo para métodos de organización de listas
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+cars.sort()
+
+print(cars)
+
+cars.sort(reverse=True)
+
+print(cars)
+
+print(len(cars))
