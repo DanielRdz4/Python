04eae44abb0b8aeaf128c225ffdfaa3531562501
Daniel <138172621+DanielRdz4@users.noreply.github.com>
2025-03-26 13:05:47 -0600

    último problema del capítulo 4

---
+++ chapter4/touple.py
@@ -0,0 +1,11 @@
+
+menu_items= ("pizza","ham","ice cream")
+
+for foods in menu_items:
+    print(foods)
+
+menu_items =("Chicken","Beef","Stew")
+
+print("\nModified menu:")
+for foods in menu_items:
+    print(foods)
