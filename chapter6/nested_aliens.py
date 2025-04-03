7462ed575f20292ea1c78e78d600832e01aa1978
Daniel <138172621+DanielRdz4@users.noreply.github.com>
2025-04-03 16:55:33 -0600

    nada

--- chapter6/nested_aliens.py
+++ chapter6/nested_aliens.py
@@ -4,8 +4,8 @@ aliens=[]
 for alien in range(30):
     new_alien = {'color':'green', 'points':5, 'speed':'slow' }
     aliens.append(new_alien)
-
 print(f'aliens created: {len(aliens)}')
-print('\nalien caracteristics:')
+
+print('alien caracteristics:')
 for caracteristics in set(new_alien.values()):
     print(caracteristics)
---
+++ chapter6/pizza_instructions.py
@@ -0,0 +1,15 @@
+#Obtener ingredientes deseados por el usuario
+ordered_toppings = []
+pizza_recipe = {
+    'crust': 0,
+    'T1': 0,
+    'T2': 0,
+    'T3': 0,
+
+}
+
+pizza_recipe['crust']=input("Tipo de crust?: ")
+pizza_recipe['T1']=input("Topping deseado: ")
+pizza_recipe['T2']=input("Topping deseado: ")
+pizza_recipe['T3']=input("Topping deseado: ")
+
