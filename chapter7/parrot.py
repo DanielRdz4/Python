ea7d56bcce554d2e50d6fa8410b9cbda10849d52
Daniel Rodríguez <138172621+DanielRdz4@users.noreply.github.com>
2025-04-04 15:41:13 -0600

    primer problema del cap 7

---
+++ chapter7/parrot.py
@@ -0,0 +1,9 @@
+
+prompt='This program will repeat anything that you type'
+prompt+='\nIf you want to quit the progtam type quit'
+print(prompt)
+message=""
+while message != 'quit':
+    message = input('I will repeat this: ')
+    if message != 'quit':
+        print(message)
